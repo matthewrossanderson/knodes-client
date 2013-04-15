2fff1e72be8be5e846f6896b9a5a8d7e0451b5d7
Matt Anderson <matthewrossanderson@gmail.com>
2013-04-15 12:45:32 -0400

    Testing modification

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
-#require_relative '../lib/knodes.rb'
 Dir[File.expand_path('../../lib/*.rb', __FILE__)].each{|f| require f}
+ENV['RACK_ENV'] = 'test'
+set :environment, :test
 
 require 'rspec'
-require 'pp'
+require 'rack/test'
+
+
